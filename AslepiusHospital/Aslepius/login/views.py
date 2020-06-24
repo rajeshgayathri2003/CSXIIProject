a666296554eb4dfbcbf0cf1249c77a554c2c7413
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-06-24 18:35:52 +0530

    the registration model

--- AslepiusHospital/Aslepius/login/views.py
+++ AslepiusHospital/Aslepius/login/views.py
@@ -4,52 +4,36 @@ from datetime import datetime
 from . import models
 from random import randint
 
-def FrontScreen(request):
-    return render(request, 'login/opening.html')
+
 
 
 def login(request):
-    return render(request,'login/login.html')
-
-
-def dept(request):
-    return render(request, 'login/departments.html')
-
-
-def cardiology(request):
-    return render(request, 'login/cardiology.html')
-
-
-def contact(request):
-    if request.method == "POST":
-        name = request.POST['name']
-        phone = request.POST['Phone']
-        country = request.POST['country']
-        message = request.POST['subject']
-        date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-        with open('contact.csv', 'a') as csvfile:
-            csvwriter=csv.writer(csvfile, lineterminator='\n')
-            csvwriter.writerow(["name", name])
-            csvwriter.writerow(["phone", phone])
-            csvwriter.writerow(["country", country])
-            csvwriter.writerow(["message", message])
-            csvwriter.writerow(["date", date])
-        return render(request, 'login/thankyou.html')
-    else:
-        return render(request, 'login/contact.html')
+    return render(request, 'login/login.html')
 
 
 def registration(request):
     if request.method == "GET":
-        return render(request,'login/registration.html')
+        return render(request, 'login/registration.html')
     else:
         name = request.POST['name']
-        #dob = request.POST['dob']
+        dob = request.POST['dob']
         #gender = request.POST['gender']  #Please check and make the necessary corrections
+        '''if request.POST['female']=="on":
+            gender="F"
+        elif request.POST['Male']=="on":
+            gender="M"
+        elif request.POST['Other']=="on":
+            gender="O"'''
+        if 'female' in request.POST:
+            gender = "F"
+        elif 'Male' in request.POST:
+            gender = "M"
+        elif 'Other' in request.POST:
+            gender = "O"
         email = request.POST['email']
         passwd = request.POST['passwd']
-        #confirmpasswd = request.POST['confirmpasswd']
-        #mobile = request.POST['mobile']
+        confirmpasswd = request.POST['confirmpasswd']
+        mobile = request.POST['mobile']
         Add1 = request.POST['Add1']
         Add2 = request.POST['Add2']
         Add3 = request.POST['Add3']
@@ -60,23 +44,25 @@ def registration(request):
         R = models.Register()
         R.patientid=randint(0,10000)
         R.name = name
-        R.dob = "2003-10-25" #Fix
-        R.gender = "F" #Fix
+        R.dob = dob  #Issue sorted
+        R.gender = gender #Minor bug- Allows multiple choices
         R.email = email
         R.passwd = passwd
-        R.confirmpasswd = passwd #Fix
-        R.mobile = 81977 #Fix
-        R.Add1 = "Add1"
-        R.Add2 = "Add2"
-        R.Add3 = "Add3"
-        # Fix
+        R.confirmpasswd = confirmpasswd
+        #Issue Sorted
+        R.mobile = mobile
+        #Issue Sorted
+        R.Add1 = Add1
+        R.Add2 = Add2
+        R.Add3 = Add3
+        # Issue sorted
         R.city = city
         R.state = state
         R.pincode = pincode
         R.save()
-        return render(request, 'login/opening.html')
+        return render(request, 'opening/opening.html')
 
-
+'''
 def dental(request):
     return render(request,'login/dental.html')
 
@@ -130,12 +116,12 @@ def psy(request):
 
 
 def pulmo(request):
-    return render(request, 'login/pulmonology.html')
+    return render(request, 'login/pulmonology.html')'''
 
-def vaccines(request):
-    return render(request,'login/vaccines.html')
+'''def vaccines(request):
+    return render(request,'login/vaccines.html')'''
 
-def covid(request):
+'''def covid(request):
     return render(request, 'login/Covid19.html')
 
 def BabyBlue(request):
@@ -152,8 +138,38 @@ def KidneyCare(request):
     return render(request,'login/KidneyCancer.html')
 
 def KidneyPain(request):
-    return render(request, 'login/KidneyPain.html')
+    return render(request, 'login/KidneyPain.html')'''
 
-def contactthankyou(request):
-    return render(request,'login/thankyou.html')
+'''def contactthankyou(request):
+    return render(request,'login/thankyou.html')'''
+
+'''def FrontScreen(request):
+    return render(request, 'login/opening.html')'''
+
+'''def dept(request):
+    return render(request, 'login/departments.html')
+
+
+def cardiology(request):
+    return render(request, 'login/cardiology.html')'''
+
+
+'''def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['Phone']
+        country = request.POST['country']
+        message = request.POST['subject']
+        date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+        with open('contact.csv', 'a') as csvfile:
+            csvwriter=csv.writer(csvfile, lineterminator='\n')
+            csvwriter.writerow(["name", name])
+            csvwriter.writerow(["phone", phone])
+            csvwriter.writerow(["country", country])
+            csvwriter.writerow(["message", message])
+            csvwriter.writerow(["date", date])
+        return render(request, 'login/thankyou.html')
+    else:
+        return render(request, 'login/contact.html')'''
+
 # Create your views here.
