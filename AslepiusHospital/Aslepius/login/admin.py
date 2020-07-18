abcc0291119754891d976c2490b517b1bfc991f1
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-07-18 18:50:30 +0530

    Removed Unnecessary files

--- AslepiusHospital/Aslepius/login/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from login.models import Register
-
-admin.site.register(Register)
-# Register your models here.
--- AslepiusHospital/Aslepius/vaccines/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [path('vaccines',views.vaccines, name='vaccines')]
