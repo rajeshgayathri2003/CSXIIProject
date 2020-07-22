76f0d442e462a9b2a018f8332adf13176e93d8ca
SByndz12 <64000773+SByndz12@users.noreply.github.com>
2020-07-22 13:16:50 +0530

    finished html linking for vaccines

--- AslepiusHospitals (Vaccines)/AslepiusHospital/Aslepius/vaccines/views.py
+++ AslepiusHospitals (Vaccines)/AslepiusHospital/Aslepius/vaccines/views.py
@@ -9,4 +9,25 @@ def birth(request):
 def two_Months(request):
     return render(request,'vaccines/2Months.html')
 
+def four_Months(request):
+    return render(request,'vaccines/4Months.html')
+
+def six_Months(request):
+    return render(request,'vaccines/6Months.html')
+
+def nine_twelve_Months(request):
+    return render(request,'vaccines/9-12Months.html')
+
+def fifteen_eighteen_Months(request):
+    return render(request,'vaccines/15-18Months.html')
+
+def two_three_Years(request):
+    return render(request,'vaccines/2-3Years.html')
+
+def five_Years(request):
+    return render(request,'vaccines/5Years.html')
+
+def seven_seventeen_Years(request):
+    return render(request,'vaccines/7-17Years.html')
+
 
