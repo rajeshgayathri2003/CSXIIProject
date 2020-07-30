9008d0f2667fa5441aadf9518cdc58f862e2c50a
SByndz12 <64000773+SByndz12@users.noreply.github.com>
2020-07-30 12:34:20 +0530

    removed

--- AslepiusHosp/Aslepius/opening/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpeningConfig(AppConfig):
-    name = 'opening'
--- AslepiusHosp/Aslepius/vaccines/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-def vaccines(request):
-    return render(request,'vaccines/vaccines.html')
-# Create your views here.
