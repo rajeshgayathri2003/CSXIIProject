027ce9f7d76c8e3f79ca5cad29528e6d79dba069
khushiKS <kks.khushi02@gmail.com>
2020-10-16 10:16:30 +0530

    change in labs

--- AslepiusHospital/Aslepius/labs/views.py
+++ AslepiusHospital/Aslepius/labs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from .models import Labs
-import csv
+from django.shortcuts import render,redirect
+from labs.models import Labs
+from django.core.mail import send_mail
+from labs.models import Labs_payment
 from login import models as loginmodels
 from appointment import models as aptmodels
 # Create your views here.
@@ -142,10 +143,11 @@ def booking(request):
 def payment(request):
     if request.method == "POST":
         fname = request.POST['firstname']
+        patientID = request.POST['patientID']
         email = request.POST['email']
         add = request.POST['address']
         city = request.POST['city']
-        testname = request.POST['testname']
+        testname = request.POST.get('testname')
         price = request.POST['price']
         state = request.POST['state']
         pincode = request.POST['Pincode']
@@ -154,25 +156,26 @@ def payment(request):
         expiry_month = request.POST['expmonth']
         expiry_year = request.POST['expyear']
         cvv = request.POST['cvv']
-        print(fname, email, 'hllo')
-        with open('payments.csv', 'a') as csvfile:
-            csvwriter=csv.writer(csvfile, lineterminator='\n')
-            csvwriter.writerow(["fname", fname])
-            csvwriter.writerow(["email", email])
-            csvwriter.writerow(["add", add])
-            csvwriter.writerow(["city", city])
-            csvwriter.writerow(["testname", testname])
-            csvwriter.writerow(["price", price])
-            csvwriter.writerow(["state", state])
-            csvwriter.writerow(["Pincode", pincode])
-            csvwriter.writerow(["cardname", cardname])
-            csvwriter.writerow(["creditcard_num", creditcard_num])
-            csvwriter.writerow(["expiry_month", expiry_month])
-            csvwriter.writerow(["expiry_year", expiry_year])
-            csvwriter.writerow(["cvv", cvv])
+        R = Labs_payment()
+        R.fname=fname
+        R.patientID = patientID
+        R.email=email
+        R.Add=add
+        R.city=city
+        R.testname=testname
+        R.price=price
+        R.state=state
+        R.pincode=pincode
+        R.cardname=cardname
+        R.cardnumber=creditcard_num
+        R.exp_month=expiry_month
+        R.exp_year=expiry_year
+        R.cvv= cvv
+        R.save()
+        body = 'Dear {0},\nYour {1} test has been succsessfully booked\n Regards,\nTeam Aselpius'.format(fname, testname)
+        send_mail('Diagnostic test payed', body,
+                  'aslepius9@gmail.com', [request.user.email, ], fail_silently=False)
         return render(request, 'labs/redirect.html')
-    else:
-        return render(request, 'labs/payment.html')
 
 def redirect(request):
     return render(request, 'labs/redirect.html')
@@ -181,8 +184,8 @@ def redirect1(request):
     username = request.user.username
     details_lst = loginmodels.Register.objects.filter(email=username)
     appointment_lst = aptmodels.Appointment.objects.filter(patientID = request.user.id)
-    return render(request, 'mypage/myhomepage.html',{'d_lst':details_lst, 'a_lst': appointment_lst})
-
+    test_lst = Labs_payment.objects.filter(patientID=request.user.id)
+    return render(request, 'mypage/myhomepage.html',{'t_lst': test_lst,'d_lst':details_lst, 'a_lst': appointment_lst})
 
 
 
