ed9e4a100f00c4ee513d25d31c4a25110794be2e
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2020-11-27 20:58:26 +0530

    Revert "trial"
    This reverts commit 7d3ea3ab283f3265646fddc9dfd7235e5a3e2e82.

--- AslepiusHospital/Aslepius/Aslepius/settings.py
+++ AslepiusHospital/Aslepius/Aslepius/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'Aslepius.wsgi.application'
 DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'GAYATHRI',
+        'NAME': 'CX11Project',
         'USER': 'root',
-        'PASSWORD': 'G@Y@3rajesh',
+        'PASSWORD': 'khushi',
         'HOST': 'localhost',
     }
  }
