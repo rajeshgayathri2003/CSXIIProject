8460a5467d4ed36964dd779272fc31cb861823ae
rajeshgayathri2003 <64058247+rajeshgayathri2003@users.noreply.github.com>
2021-09-08 13:06:48 +0530

    Just looked at our project!

--- AslepiusHospital/Aslepius/Aslepius/settings.py
+++ AslepiusHospital/Aslepius/Aslepius/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = (
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "aslepius9@gmail.com"
-EMAIL_HOST_PASSWORD = 'Aslepius@123'
+EMAIL_HOST_PASSWORD = 'Aslepius_123'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
